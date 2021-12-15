31ea6f57309931c8a225604bc85d34a54cceb4c6
t1dmlgus <dmlgusgngl@gmail.com>
2021-12-16 08:20:20 +0900

    Initial commit

---
+++ src/test/java/com/s1dmlgus/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
